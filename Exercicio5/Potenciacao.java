3183d2322ed0b0956bfe3f54429b7789c749774d
Wess <118078373+WezleyRenan@users.noreply.github.com>
2023-04-02 16:29:54 -0300

    melhorei o codigo da 5 questao

--- Exercicio5/Potenciacao.java
+++ Exercicio5/Potenciacao.java
@@ -2,17 +2,14 @@ package Exercicio5;
 
 public class Potenciacao {
 
-	public int potencia(int n, int p) {
-		
-		if (p < 1 && p > 0) {
-			return n * potencia(n,p - 1);
-		}
-		if (p == 0) {
-			return 1;
-		}
-		return n;
+	public int potencia(int n, int p) throws Exception {
 
-		
+		if (p > 0) {
+			return n * potencia(n, p - 1);
+		}
+		if (p < 0) {
+			throw new Exception("expoente negativo erro!!!");
+		}
+		return 1;
 	}
-
 }
--- Exercicio5/Principal.java
+++ Exercicio5/Principal.java
@@ -4,17 +4,21 @@ import java.util.Scanner;
 
 public class Principal {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		int n, pow;
-	    
+
 		Potenciacao conta = new Potenciacao();
-		Scanner input = new Scanner (System.in);
+		Scanner input = new Scanner(System.in);
 		System.out.println("digite um numero que voce deseja operar:");
 		n = input.nextInt();
 		System.out.println("digite o numero do expoente");
-        pow = input.nextInt();
+		pow = input.nextInt();
 
-		System.out.println(conta.potencia(n,pow));
+		try {
+			System.out.println(conta.potencia(n, pow));
+		} catch (Exception erro) {
+			System.out.println(erro.getMessage());
+		}
 
 	}
 
