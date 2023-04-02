edbf07b0c5dbae22e5cafda9bf40a222472e12e1
Wess <118078373+WezleyRenan@users.noreply.github.com>
2023-04-02 15:36:18 -0300

    modifiquei o erro na classe contagem do 4 exercicio

--- Exercicio4/Contagem.java
+++ Exercicio4/Contagem.java
@@ -2,13 +2,16 @@ package Exercicio4;
 
 public class Contagem {
 
-	public int cont(int n,int m) {
+	public int cont(int n, int m) {
 
 		if (n < m) {
 			System.out.println(n);
-			return cont(n + 2,m);
+			return cont(n + 2, m);
 
 		} else {
+			if (n > m) {
+				return n - 1;
+			}
 			return n;
 		}
 
