a42b5f536bb7681ebc51e2edef59cad24e1e8c49
Wess <118078373+WezleyRenan@users.noreply.github.com>
2023-04-02 15:38:49 -0300

    concertei outro erro que acontecia com a contagem ao colocar numeros impares como base

--- Exercicio4/Contagem.java
+++ Exercicio4/Contagem.java
@@ -4,13 +4,13 @@ public class Contagem {
 
 	public int cont(int n, int m) {
 
-		if (n < m) {
+		if (n < (m - 1)) {
 			System.out.println(n);
 			return cont(n + 2, m);
 
 		} else {
 			if (n > m) {
-				return n - 1;
+				return n - 2;
 			}
 			return n;
 		}
